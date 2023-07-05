0f3ce11092225479a88a5a8595d634853b0dfb18
Cholaraja <madhureddy88@gmail.com>
2023-07-05 15:58:28 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,22 @@
 import os
 
-def sort_file(dir_path):
+def sort_file(source_path, dest_path):
     global_content = []
-    for file_ in os.listdir(dir_path):
+    for file_ in os.listdir(source_path):
         #read the file for contents
-        with open(dir_path +file_, 'r') as f:
+        with open(source_path +file_, "r") as f:
             for line in  f.readlines():
                 # print(line)
                 line = line.strip()
                 if not line: continue
-                (t, u) = line.split(' ')
+                (t, u) = line.split(" ")
                 # collect all the file contents
                 global_content.append((int(t), u))
     # sort global content by key
-    return sorted(global_content, key=lambda x: x[0])
+    sorted_content = sorted(global_content, key=lambda x: x[0])
+    with open(dest_path +"-".join(os.listdir(source_path)) +"-merged.log", "w+") as f:
+        for tup in sorted_content:
+            f.write(" ".join([str(i) for i in tup])+ "\n")
 
-
-if __name__ == '__main__':
-   print(sort_file(os.path.realpath('.')+'/files/'))
+if __name__ == "__main__":
+   sort_file(os.path.realpath(".")+"/files/", os.path.realpath(".")+"/files_p/")
